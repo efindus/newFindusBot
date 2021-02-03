8942812463d0a6c8205941570ab30c86813744f8
Findus <48939809+efindus@users.noreply.github.com>
2021-02-03 15:46:34 +0100

    fix

--- plugins/statistics/commands/hypixel.js
+++ plugins/statistics/commands/hypixel.js
@@ -7,82 +7,90 @@ const mojangAPI = 'https://api.mojang.com/users/profiles/minecraft/'
 var translations = {en: [], pl: [], ru: []}
 
 exports.output = async ({message, guild, args}) => {
-    let user = args[0]
-    let stats = args.length > 1 ? args[1] : 'general'
-    translations.pl[0] = `${ef.emotes.markNo}Podaj poprawną nazwę użytkownika!`
-    translations.en[0] = `${ef.emotes.markNo}Please enter a valid username!`
-    translations.ru[0] = `${ef.emotes.markNo}Пожалуйста введите действительное имя пользователя!`
-    if(/[^a-z0-9_]/i.test(user)) return ef.models.send({object: message, message: `${translations[guild.settings.language][0]}`, color: ef.colors.red})
-    
-    let desiredEndpoint = ''
+    try {
+        let user = args[0]
+        let stats = args.length > 1 ? args[1] : 'general'
+        translations.pl[0] = `${ef.emotes.markNo}Podaj poprawną nazwę użytkownika!`
+        translations.en[0] = `${ef.emotes.markNo}Please enter a valid username!`
+        translations.ru[0] = `${ef.emotes.markNo}Пожалуйста введите действительное имя пользователя!`
+        if(/[^a-z0-9_]/i.test(user)) return ef.models.send({object: message, message: `${translations[guild.settings.language][0]}`, color: ef.colors.red})
 
-    switch(stats) {
-        case 'general':
-            desiredEndpoint = 'general'
-            break;
-        case 'general-info':
-            desiredEndpoint = 'general-tooltip'
-            break;
-        case 'bedwars':
-            desiredEndpoint = 'bedwars'
-            break;
-        case 'duels':
-            desiredEndpoint = 'duels'
-            break;
-        case 'skywars':
-            desiredEndpoint = 'skywars?show_survived_players=true'
-            break;
-        case 'guild':
-            desiredEndpoint = 'guild/general'
-            break;
-        default:
-            return ef.models.send({
+        let desiredEndpoint = ''
+
+        switch(stats) {
+            case 'general':
+                desiredEndpoint = 'general'
+                break;
+            case 'general-info':
+                desiredEndpoint = 'general-tooltip'
+                break;
+            case 'bedwars':
+                desiredEndpoint = 'bedwars'
+                break;
+            case 'duels':
+                desiredEndpoint = 'duels'
+                break;
+            case 'skywars':
+                desiredEndpoint = 'skywars?show_survived_players=true'
+                break;
+            case 'guild':
+                desiredEndpoint = 'guild/general'
+                break;
+            default:
+                return ef.models.send({
+                    object: message,
+                    message: `${ef.emotes.markNo}Select correct statistic to check!`,
+                    color: ef.colors.red
+                })
+        }
+
+        const response = await ef.http.get(`${planckeBaseURL}${user}.png`);
+
+        if(Buffer.from(response.body).toString('utf8') == "ensure==BadPlayerException"){
+            translations.pl[0] = `${ef.emotes.markNo}Użytkownik nie istnieje!`
+            translations.en[0] = `${ef.emotes.markNo}User does not exist!`
+            translations.ru[0] = `${ef.emotes.markNo}Пользователь не существует!`
+            ef.models.send({
                 object: message,
-                message: `${ef.emotes.markNo}Select correct statistic to check!`,
+                message: `${translations[guild.settings.language][0]}`,
                 color: ef.colors.red
             })
-    }
+        } else {
+            const mojangAPIResponse = await ef.http.get(`${mojangAPI}${user}`)
 
-    const response = await ef.http.get(`${planckeBaseURL}${user}.png`);
+            const UUID = mojangAPIResponse.body.id
 
-    if(Buffer.from(response.body).toString('utf8') == "ensure==BadPlayerException"){
-        translations.pl[0] = `${ef.emotes.markNo}Użytkownik nie istnieje!`
-        translations.en[0] = `${ef.emotes.markNo}User does not exist!`
-        translations.ru[0] = `${ef.emotes.markNo}Пользователь не существует!`
+            const paniekAPIResponse = await ef.http.get(`${paniekBaseURL}${UUID}/${desiredEndpoint}`)
+
+            let img = new canvas.Image()
+            img.src = paniekAPIResponse.body
+
+            let can = canvas.createCanvas(img.width, img.height)
+            let ctx = can.getContext('2d')
+            ctx.fillStyle = '#ffffff'
+            ctx.fillRect(0, 0, img.width, img.height)
+            ctx.drawImage(img, 0, 0, img.width, img.height)
+            can.createPNGStream()
+
+            const file = new Attachment(can.createPNGStream(), `${user}.png`)
+            
+            translations.pl[0] = `Statystyki dla ${user}`
+            translations.en[0] = `Stats for ${user}`
+            translations.ru[0] = `Статистика для ${user}`
+            
+            ef.models.send({
+                object: message,
+                message: ``,
+                file: file,
+                image: `attachment://${user}.png`,
+                footer: `${translations[guild.settings.language][0]}`
+            })
+        }
+    } catch (e) {
         ef.models.send({
             object: message,
-            message: `${translations[guild.settings.language][0]}`,
+            message: `${ef.emotes.markNo}Oops something went wrong... Try again later.`,
             color: ef.colors.red
-        })
-    } else {
-        const mojangAPIResponse = await ef.http.get(`${mojangAPI}${user}`)
-
-        const UUID = mojangAPIResponse.body.id
-
-        const paniekAPIResponse = await ef.http.get(`${paniekBaseURL}${UUID}/${desiredEndpoint}`)
-
-        let img = new canvas.Image()
-        img.src = paniekAPIResponse.body
-
-        let can = canvas.createCanvas(img.width, img.height)
-        let ctx = can.getContext('2d')
-        ctx.fillStyle = '#ffffff'
-        ctx.fillRect(0, 0, img.width, img.height)
-        ctx.drawImage(img, 0, 0, img.width, img.height)
-        can.createPNGStream()
-
-        const file = new Attachment(can.createPNGStream(), `${user}.png`)
-        
-        translations.pl[0] = `Statystyki dla ${user}`
-        translations.en[0] = `Stats for ${user}`
-        translations.ru[0] = `Статистика для ${user}`
-        
-        await ef.models.send({
-            object: message,
-            message: ``,
-            file: file,
-            image: `attachment://${user}.png`,
-            footer: `${translations[guild.settings.language][0]}`
         })
     }
 }
