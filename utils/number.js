b5f762b9f86bb90fe802b19d0c8084d5ef14b28c
efindus <48939809+efindus@users.noreply.github.com>
2021-03-30 10:40:40 +0200

    Update number.js

--- utils/number.js
+++ utils/number.js
@@ -2,5 +2,5 @@ const { randomBytes } = require("crypto");
 
 exports.random = async (Min, Max) =>
 {
-    return Min + Math.floor(randomBytes(4).readUInt32LE() / 0xffffffff * (Max - Min + 1));
+    return Min + Math.round(randomBytes(4).readUInt32LE() / 0xffffffff * (Max - Min + 1));
 }
